e76596b6096d5b4b06a4ece807e8e097cba9f9a3
Maciej Koziel <koziel.maciej@outlook.com>
2017-08-16 17:40:01 +0200

    Serial log steering.

--- rc-controller/rc-controller.ino
+++ rc-controller/rc-controller.ino
@@ -9,17 +9,17 @@
 #define THROTTLE_ZERO 1470
 
 int ch1;
-int p_ch1 = 200;
+int steering_value = 0;
 const int alpha_ch1 = 25;
 Servo ch1_out;
 
 int ch2;
-int p_ch2 = 0;
+int throttle_value = 0;
 const int alpha_ch2 = 40;
 Servo ch2_out;
 
 int t_last_log = 0;
-int t_log_freq = 250;
+int t_log_freq = 50;
 
 void setup() {
   ch1_out.attach(SERVO_PIN_OUT);
@@ -31,28 +31,23 @@ void setup() {
 
 void loop() { 
   ch1 = pulseIn(SERVO_PIN_IN, HIGH); // Read the pulse width of 973 - 1954 for steering
-  if (abs(ch1 - p_ch1) > alpha_ch1) {
-    p_ch1 = ch1;  
-    ch1_out.writeMicroseconds(p_ch1);
+  if (abs(ch1 - steering_value) > alpha_ch1) {
+    steering_value = ch1;  
+    ch1_out.writeMicroseconds(steering_value);
   }
   
   ch2 = pulseIn(ESC_PIN_IN, HIGH); // Read the pulse width of 973 - 1954 for thr
   if (ch2 > THROTTLE_ZERO + alpha_ch2 || ch2 < THROTTLE_ZERO - alpha_ch2) {
-    p_ch2 = ch2;
+    throttle_value = ch2;
   } else {
-    p_ch2 = THROTTLE_ZERO;
+    throttle_value = THROTTLE_ZERO;
   }
-  ch2_out.writeMicroseconds(p_ch2);
+  ch2_out.writeMicroseconds(throttle_value);
   
   int t = millis();
   if (t - t_last_log > t_log_freq) {
     t_last_log = t;
-    String log_out = "{\"steering\": ";
-    log_out += p_ch1;
-    log_out += ", \"acceleration\": ";
-    log_out += p_ch2;
-    log_out += "}";
-    Serial.println(log_out);
+    Serial.println(steering_value);
   }
   
   delay(10);
