745b406d19a2267f45c7263acb2a836a91b409ca
Pi <pi@pi>
2017-08-17 12:49:12 +0000

    Process graceful shutdown

--- raspberry/save.py
+++ raspberry/save.py
@@ -4,51 +4,63 @@ from picamera import PiCamera
 from time import sleep
 import serial
 import time
-from multiprocessing import Process
+from multiprocessing import Process, Value
 import signal
+import sys
 
-term = False
+global camera_process
+global serial_process
+global terminated 
 
 def timestamp():
-	return int(round(time.time() * 1000))
+    return int(round(time.time() * 1000))
 
 def filenames():
     frame = 0
-    while True:
+    while terminated.value == 0:
         yield 'images/%d.jpg' % timestamp()
 
-def soft_kill(signum, frame):
-    print("signal received\n")
-    term = True
-
 def capture():
+    signal.signal(signal.SIGINT, signal.SIG_IGN)
     with PiCamera(resolution=(640, 480), framerate=15) as camera:
         camera.start_preview()
         sleep(2)
         camera.capture_sequence(filenames(), use_video_port=True)
 
-def kill(proc_pid):
-    process = psutil.Process(proc_pid)
-    for proc in process.children(recursive=True):
-        proc.kill()
-    process.kill()
+def read_serial():
+    signal.signal(signal.SIGINT, signal.SIG_IGN)
+    ser = serial.Serial('/dev/ttyACM0', 9600)
+    with open("log", "w") as f:
+        while terminated.value == 0:
+            read_serial = ser.readline()
+            f.write(str(timestamp()) + "/" +  read_serial)
 
-for file in glob("images/*.jpg"):
-    os.remove(file)
+def interrupt_handler(signal, frame):
+    terminated.value = 1
 
-p = Process(target=capture)
-p.start()
+def cleanup():
+    try:
+        os.remove("log")
+        for file in glob("images/*.jpg"):
+            os.remove(file)
+    except OSError:
+        pass
 
-signal.signal(signal.SIGINT, soft_kill)
+# main
 
-ser = serial.Serial('/dev/ttyACM0',9600)
-print("read serial communication")
-with open("log", "w") as f:
-    while not term:
-        read_serial = ser.readline()
-        f.write(str(timestamp()) + "/" +  read_serial)
+terminated = Value('i', 0)
 
-print("terminating\n")
-print(p.pid)
-kill(p.pid)
+camera_process = Process(target=capture)
+camera_process.daemon = True
 
+serial_process = Process(target=read_serial)
+serial_process.daemon = True
+
+cleanup()
+camera_process.start()
+serial_process.start()
+
+signal.signal(signal.SIGINT, interrupt_handler)
+
+camera_process.join()
+serial_process.join()
