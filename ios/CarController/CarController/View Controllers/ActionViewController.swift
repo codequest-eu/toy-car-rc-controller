3a625c465ad4626621fa5e31c0903d6859ca8ecb
mdziubich <gosia.dziubich@codequest.com>
2017-09-23 18:58:43 +0200

    seup flow for run button

--- ios/CarController/CarController/View Controllers/ActionViewController.swift
+++ ios/CarController/CarController/View Controllers/ActionViewController.swift
@@ -10,14 +10,72 @@ import UIKit
 
 class ActionViewController: UIViewController {
     
+    @IBOutlet weak var runButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var statusLabel: UILabel!
+    
     enum ActionStatus {
-        case idle, running, paused
+        case idle, running, paused, recording
+    }
+    
+    var status: ActionStatus = .idle {
+        didSet {
+            
+        }
     }
 
+    //MARK: View life cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
+    
+    //MARK: Actions
 
+    @IBAction func run(_ sender: Any) {
+        switch status {
+        case .idle:
+            status = .running
+        case .running:
+            status = .paused
+        case .paused:
+            status = .running
+        case .recording: return
+        }
+    }
+    
+    @IBAction func record(_ sender: Any) {
+//        switch status {
+//        case .idle:
+//
+//        case .running:
+//
+//        case .paused:
+//
+//        case .recording:
+//
+//        }
+    }
+    
+    //MARK: Private functions
+    
+    private func updateButtonsState() {
+        switch status {
+        case .idle:
+            updateButtonsImage(for: [(runButton, #imageLiteral(resourceName: "play-button")), (recordButton, #imageLiteral(resourceName: "record"))])
+        case .running:
+            updateButtonsImage(for: [(runButton, #imageLiteral(resourceName: "pause")), (recordButton, #imageLiteral(resourceName: "record"))])
+        case .paused:
+            updateButtonsImage(for: [(runButton, #imageLiteral(resourceName: "play-button")), (recordButton, #imageLiteral(resourceName: "record"))])
+        case .recording:
+            updateButtonsImage(for: [(runButton, #imageLiteral(resourceName: "play-button")), (recordButton, #imageLiteral(resourceName: "record"))]) //todo: update recordButton
+        }
+    }
+    
+    private func updateButtonsImage(for buttonsArray: [(button: UIButton, image: UIImage)]) {
+        buttonsArray.forEach {
+            $0.button.setImage($0.image, for: .normal)
+        }
+    }
 }
