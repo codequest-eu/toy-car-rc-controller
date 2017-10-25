53e738bb276f076faf71fbe38c8fca88af1e4e4d
Pi <pi@pi>
2017-10-25 16:29:58 +0000

    working?

--- raspberry/neural_network_predictor.py
+++ raspberry/neural_network_predictor.py
@@ -1,12 +1,38 @@
-import string
+import sys
+sys.path.append('/root/miniconda3/lib/python3.4/site-packages')
+
+from keras.models import model_from_json
+from keras.models import load_model
+from keras.optimizers import Adam
+import numpy as np
 import time
+import h5py
+import cv2
+import imutils
 
 class NeuralNetworkPredictor:
 
     def __init__(self):
-        pass
+        print('loading model')
+        with open('out.json', encoding='utf-8') as net:
+            model_text = net.read()
+            self.model = model_from_json(model_text)
+        print('model loaded, loading weights')
+        self.model.load_weights('out.h5')
+        print('weights loaded')
 
     def predict(self, frame):
-        time.sleep(0.01)
-        return 1470
+        return self.predict_angle(frame.array)
+
+    def preprocess_image(self, image):
+        image = image[:, :320]
+        image = imutils.rotate_bound(image, 270)
+        image = cv2.resize(image, (64, 64), cv2.INTER_AREA)
+        return cv2.cvtColor(image, cv2.COLOR_RGB2HSV)[:,:,1]
+
+    def predict_angle(self, image):
+        image = self.preprocess_image(image)
+        result = float(self.model.predict(image.reshape(1, 64, 64, 1), batch_size=1))
+        interpolated = 1470 + result * 490
+        return int(interpolated)
 
