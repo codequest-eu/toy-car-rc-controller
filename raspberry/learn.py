e5f0acb10facaf650d12a99d501034ac04b0730d
Maciej Koziel <maciejkoziel@Maciejs-MacBook-Pro.local>
2017-10-27 12:20:15 +0200

    Learn script parameters, saving weights and modern Keras syntax.

--- raspberry/learn.py
+++ raspberry/learn.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+import argparse
 from keras.models import Model
 from keras.layers import Dense, Dropout, ELU, Lambda, merge, MaxPooling2D, Input, Activation, GlobalAveragePooling2D
 from keras.layers import concatenate
@@ -14,6 +16,11 @@ LEARNING_RATE = 1e-1
 BATCH_SIZE = 128
 EPOCHS = 50
 
+def parse_dirnames():
+    parser = argparse.ArgumentParser(description='Correlates steering with images')
+    parser.add_argument('dirnames', metavar='dirname', nargs='+', help='a directory with saved session')
+    return parser.parse_args().dirnames
+
 def fire_module(x, fire_id, squeeze=16, expand=64):
     """
     This is a modified version of: https://github.com/rcmalli/keras-squeezenet/blob/master/squeezenet.py#L14
@@ -28,13 +35,13 @@ def fire_module(x, fire_id, squeeze=16, expand=64):
     exp3x3 = "expand3x3"
     elu = "elu_"
 
-    x = Convolution2D(squeeze, 1, 1, border_mode='valid', name=s_id + sq1x1)(x)
+    x = Convolution2D(squeeze, (1, 1), padding='valid', name=s_id + sq1x1)(x)
     x = Activation('elu', name=s_id + elu + sq1x1)(x)
 
-    left = Convolution2D(expand, 1, 1, border_mode='valid', name=s_id + exp1x1)(x)
+    left = Convolution2D(expand, (1, 1), padding='valid', name=s_id + exp1x1)(x)
     left = Activation('elu', name=s_id + elu + exp1x1)(left)
 
-    right = Convolution2D(expand, 3, 3, border_mode='same', name=s_id + exp3x3)(x)
+    right = Convolution2D(expand, (3, 3), padding='same', name=s_id + exp3x3)(x)
     right = Activation('elu', name=s_id + elu + exp3x3)(right)
 
     # x = merge([left, right], mode='concat', concat_axis=c_axis, name=s_id + 'concat')
@@ -51,7 +58,7 @@ def squeeze_model_52():
     input_img = Input(shape=input_shape)
     x = Lambda(lambda x: x/127.5 - 1.,input_shape=input_shape)(input_img)
 
-    x = Convolution2D(2, 3, 3, subsample=(2, 2), border_mode='valid', name='conv1')(x)
+    x = Convolution2D(2, (3, 3), strides=(2, 2), padding='valid', name='conv1')(x)
     x = Activation('elu', name='elu_conv1')(x)
     x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='pool1')(x)
 
@@ -61,7 +68,7 @@ def squeeze_model_52():
 
     x = GlobalAveragePooling2D()(x)
     out = Dense(1, name='loss')(x)
-    model = Model(input=input_img, output=[out])
+    model = Model(inputs=[input_img], outputs=[out])
 
     model.compile(optimizer=Adam(lr=LEARNING_RATE), loss='mse')
     return model
@@ -69,13 +76,14 @@ def squeeze_model_52():
 if __name__ == '__main__':
     model = squeeze_model_52()
 
-    features, labels = prepare_from_paths(['session-1', 'session-1']) # parse from args
+    features, labels = prepare_from_paths(parse_dirnames())
     features = np.array(features).reshape(len(features), 64, 64, 1)
     model.fit(x=features,
               y=labels,
               verbose=1,
               batch_size=BATCH_SIZE,
-              nb_epoch=EPOCHS,
+              epochs=EPOCHS,
               validation_split=0.3)
 
+    model.save_weights('out.h5')
     print("Training complete!")
